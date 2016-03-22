7cf07503bfcd7e99ccd194b701d4ba5e18ca2821
KimYebon <v954242@naver.com>
2016-03-22 14:32:06 +0900

    add flask_restful

--- youngs_server/model/UserChannel.py
+++ youngs_server/model/UserChannel.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, VARCHAR
 from sqlalchemy.orm import relationship
 from youngs_server.model import Base
 from youngs_server.model import User
 from youngs_server.model import Channel
-
 
 class UserChannel(Base):
     __tablename__ = 'userChannel'
@@ -14,14 +13,17 @@ class UserChannel(Base):
 
     userId = Column(Integer, ForeignKey(User.userId))
     channelId = Column(Integer, ForeignKey(Channel.channelId))
+    #d - default, f-favorite, r-read
+    type = Column(VARCHAR, unique=False)
 
     user = relationship('User', backref='userChannel')
     channel = relationship('Channel', backref='userChannel')
 
     # id는 자동생성
-    def __init__(self, userId, channelId):
+    def __init__(self, userId, channelId, type):
         self.userId = userId
         self.channelId = channelId
+        self.type = type
 
     def __repr__(self):
         return '<Channel %r %r>' % (self.userId, self.channelId)
--- youngs_server/youngs_blueprint.py
+++ youngs_server/youngs_blueprint.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-    photolog.blueprint
-    ~~~~~~~~~~~~~~~~~~
-    photolog 어플리케이션에 적용할 blueprint 모듈.
-    :copyright: (c) 2013 by 4mba.
-    :license: MIT LICENSE 2.0, see license for more details.
-"""
-
 
 from flask import Blueprint
 from youngs_server.youngs_logger import Log
 
-youngs = Blueprint('youngs', __name__,
-                     template_folder='../templates', static_folder='../static')
+youngs = Blueprint('youngs', __name__, static_folder='../static')
 
 Log.info('static folder : %s' % youngs.static_folder)
-Log.info('template folder : %s' % youngs.template_folder)a
